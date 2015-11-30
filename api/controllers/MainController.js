ac474f02ff7aebeecaf1747a6d283a9f00c9e6ab
el-sonny <sonny@spaceshiplabs.com>
2015-11-30 12:09:55 -0600

    basic configuration

---
+++ api/controllers/MainController.js
@@ -0,0 +1,12 @@
+/**
+ * MainController
+ *
+ * @description :: Server-side logic for managing mains
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+  index: function(req, res) {
+    res.json('hi');
+  }
+};
